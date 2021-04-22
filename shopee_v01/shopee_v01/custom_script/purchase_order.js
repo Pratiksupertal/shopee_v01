b0d929fdf9e88804ce4677d9ed53929488be33ef
Pratiksupertal <pratik@supertal.io>
2021-04-22 16:18:15 +0530

    Purchase order changes

--- shopee_v01/shopee_v01/custom_script/purchase_order.js
+++ shopee_v01/shopee_v01/custom_script/purchase_order.js
@@ -1,4 +1,13 @@
 frappe.ui.form.on('Purchase Order', {
+	supplier:function(frm){
+		frappe.call({
+      method: "shopee_v01.shopee_v01.custom_script.purchase_order.warehouse_filter",
+      args: {"template_name":frm.doc.supplier},
+      callback: function(r) {
+           var resp = r.message
+  }
+});
+	},
 	cara_packing_template : function(frm){
     frappe.call({
       method: "shopee_v01.shopee_v01.custom_script.purchase_order.cara_packing",
--- shopee_v01/shopee_v01/custom_script/purchase_order.py
+++ shopee_v01/shopee_v01/custom_script/purchase_order.py
@@ -2,19 +2,20 @@ from frappe.model.naming import make_autoname
 import frappe
 from frappe.model.document import Document
 
+
 def autoname(doc,method):
     if doc.is_new():
         po_type = doc.po_type
         if po_type:
-            print(po_type[0])
-            a = po_type.split(" ")
-            print("---------------------------")
-            doc.name = make_autoname(a[1][0]+"PO" + "-.#####")
+            doc.name = make_autoname(po_type +".YYYY"+".MM."+ "-.####")
         else:
-            doc.name = make_autoname("PO" + "-.#####")
+            doc.name = make_autoname("PO" +".YYYY.MM."+ "-.####")
     #shopee_v01.shopee_v01.custom_script.purchase_order.test
     #shopee_v01/shopee_v01/custom_script/purchase_order.js
 
+@frappe.whitelist()
+def warehouse_filter(supplier):
+    print("---------------")
 
 
 @frappe.whitelist()
