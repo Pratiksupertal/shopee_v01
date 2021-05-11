454a11bc788275dc26643e1591e6f88f219c9c8d
Pratiksupertal <pratik@supertal.io>
2021-05-11 16:21:23 +0530

    code cleared

--- shopee_v01/shopee_v01/custom_script/supplier.py
+++ shopee_v01/shopee_v01/custom_script/supplier.py
@@ -5,13 +5,6 @@ import json
 
 @frappe.whitelist()
 def available_credit(supplier):
-    print("-------supplier doctype frappe call execcuted---------",supplier)
-    # doc = frappe.get_doc('Purchase Order',{"supplier":supplier})
-    # if(doc)
     sql = "select sum(total) as total from `tabPurchase Order` where supplier = '{0}' and status in ('To Bill','To Receive and Bill') ".format(supplier)
     res = frappe.db.sql(sql);
-    # res = json.loads(res[0])
-    print("========================================",type(res))
-
-    print(res[0])
     return res[0]
