988a4aa85533f900a2e754ab02e9434e99025ce7
siddhanth121 <siddhanth@supertal.io>
2021-05-19 10:10:38 +0530

    removed outdated imports

--- shopee_v01/__init__.py
+++ shopee_v01/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from shopee_v01.api.v1.api2 import sales_order
-from shopee_v01.api.v1.api import ping
-from shopee_v01.api.v1.api import sales_order
 
 __version__ = '0.0.1'
