651dbb13502e9ae355019611d18164067d9484cf
siddhanth121 <siddhanth@supertal.io>
2021-05-27 08:43:32 +0530

    api v4

---
+++ shopee_v01/api/v1/api3.py
@@ -0,0 +1,468 @@
+import json
+
+import frappe
+import requests
+from PIL import Image, ImageDraw, ImageFont
+import base64
+import os
+import barcode
+from urllib.parse import urlparse
+
+base = ''
+
+
+def get_request(request):
+    global base
+    parts = urlparse(request.url)
+    base = parts.scheme + '://' + parts.hostname + (':' + str(parts.port)) if parts.port != '' else ''
+    cookies = request.cookies
+    return cookies
+
+
+def validate_data(data):
+    if len(data) == 0 or data is None:
+        return None
+    try:
+        data = json.loads(data)
+        return data
+    except ValueError:
+        return "Invalid JSON submitted"
+
+
+def query_db(doctype, filters=None, fields=None):
+    check = frappe.db.get_list(
+        doctype,
+        fields=fields,
+        filters=filters)
+    return check
+
+
+def post_processing(res):
+    if res.status_code != 200:
+        raise requests.exceptions.HTTPError
+    return res.json()
+
+
+def get_document(doctype, cookies, fields=None, filters=None):
+    global base
+    if not cookies:
+        return {'message': 'Credentials not identified. Please login first.'}
+    url = base + '/api/resource/' + doctype
+
+    if filters and fields:
+        url += '?filters=' + str(filters)
+        url += '&fields=' + str(fields)
+    else:
+        if fields:
+            url += '?fields=' + str(fields)
+        elif filters:
+            url += '?filters=' + str(filters)
+
+    res = requests.get(url.replace("'", '"'), cookies=cookies)
+    print(res)
+    return post_processing(res)
+
+
+def convert_to_pdf(template=None, invoice=None, weight=None, shipping=None, to_entity=None,
+                   from_entity=None, address=None, address_company=None, product_list1=None,
+                   delivery_type=None, b_code=None):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    image = Image.open(dir_path + '/Tokopedia Label edit.jpg')
+    draw = ImageDraw.Draw(image)
+    color = 'rgb(0, 0, 0)'
+    font = ImageFont.truetype(dir_path + '/OpenSans-Semibold.ttf', size=15)
+    (x, y) = (320, 27)
+    # template = "Full Payment - COD"
+    draw.text((x, y), template, fill=color, font=font)
+
+    upc = barcode.get('upc', b_code, writer=barcode.writer.ImageWriter())
+    img = upc.render()
+    size = 280, 128
+    image.paste(img.resize(size, Image.ANTIALIAS), (190, 67))
+
+    font = ImageFont.truetype(dir_path + '/OpenSans-Semibold.ttf', size=12)
+
+    (x, y) = (50, 70)
+    # invoice = "Invoice"
+    draw.text((x, y), invoice, fill=color, font=font)
+
+    (x, y) = (32, 162)
+    # weight = "weight"
+    draw.text((x, y), weight, fill=color, font=font)
+
+    (x, y) = (118, 103)
+    # delivery_type = "Regular shipping"
+    draw.text((x, y), delivery_type, fill=color, font=font)
+
+    (x, y) = (116, 162)
+    # shipping = "shipping"
+    draw.text((x, y), shipping, fill=color, font=font)
+
+    (x, y) = (32, 260)
+    # to_entity = "to entity"
+    draw.text((x, y), to_entity, fill=color, font=font)
+
+    (x, y) = (245, 260)
+    # from_entity = "from entity"
+    draw.text((x, y), from_entity, fill=color, font=font)
+
+    font = ImageFont.truetype(dir_path + '/OpenSans-Light.ttf', size=10)
+
+    (x, y) = (32, 278)
+    # address = "Jl. Customer 2 no 2<br>Bogor<br>\nJawa Barat<br>16000<br>Indonesia<br>\n"
+    draw.text((x, y), address.replace('\n', '').replace('<br>', '\n'), fill=color, font=font)
+
+    (x, y) = (245, 278)
+    # address_company = "Jl. Customer 2 no 2<br>Bogor<br>\nJawa Barat<br>16000<br>Indonesia<br>\n"
+    draw.text((x, y), address_company.replace('\n', '').replace('<br>', '\n'), fill=color, font=font)
+
+    (x1, y1) = (32, 400)
+    (x2, y2) = (420, 400)
+    for i in product_list1:
+        draw.text((x1, y1), i['item_name'], fill=color, font=font)
+        draw.text((x2, y2), str(i['qty']) + ' Pcs', fill=color, font=font)
+        (x1, y1), (x2, y2) = (x1, y1 + 15), (x2, y2 + 15)
+
+    # out = io.BytesIO()
+    # image.save(out, format='pdf')
+    # out1 = base64.base64encode(out)
+    # print(out.getvalue().decode())
+
+    image.save(dir_path + '/output.pdf', resolution=500)
+    with open(dir_path + "/output.pdf", "rb") as pdf_file:
+        encoded_string = base64.b64encode(pdf_file.read())
+
+    return encoded_string
+
+# import os
+# with open(os.path.expanduser('test.pdf'), 'wb') as fout:
+#      fout.write(base64.decodebytes(encoded_string))
+
+
+@frappe.whitelist(allow_guest=True)
+def test():
+    cookies = get_request(frappe.request)
+    return get_document('Purchase Order', cookies=cookies)
+
+
+@frappe.whitelist()
+def purchases():
+    cookies = get_request(frappe.request)
+
+    purchase_ids = get_document('Purchase Order', cookies=cookies)
+    result = []
+
+    for i in purchase_ids['data']:
+        each_data = frappe.get_doc(
+            'Purchase Order',
+            i['name']
+        )
+
+        temp_dict = {
+            "id": each_data.idx,
+            "po_number": each_data.name,
+            "po_date": each_data.creation,
+            "supplier_id": each_data.supplier,
+            "supplier_name": each_data.supplier_name,
+            "total_amount": each_data.grand_total,
+            "total_product": each_data.total_qty,
+            "products": [{
+                "id": i.idx,
+                "purchase_id": i.parent,
+                "product_id": i.item_code,
+                "product_name": i.item_name,
+                "product_code": i.item_code,
+                "price": i.amount,
+                "quantity": i.qty,
+                "unit_id": i.idx,
+                "discount": i.discount_amount,
+                "subtotal_amount": i.net_amount
+            } for i in each_data.items],
+            "type": each_data.po_type,
+            "rejected_by": each_data.modified_by if each_data.docstatus == 2 else None,
+            "cancelled_by": each_data.modified_by if each_data.status == 2 else None,
+            "supplier_is_taxable": None,
+            "total_amount_excluding_tax": each_data.base_total,
+            "tax_amount": each_data.total_taxes_and_charges,
+            "delivery_contact_person": None,
+            "supplier_email": None,
+            "supplier_work_phone": None,
+            "supplier_cell_phone": None,
+            "expiration_date": each_data.schedule_date,
+            "payment_due_date": None if len(each_data.payment_schedule) == 0 else each_data.payment_schedule[
+                0].due_date,
+            "notes": each_data.remarks,
+            "rejection_notes": each_data.remarks if each_data.docstatus == 2 else None,
+            "cancellation_notes": each_data.remarks if each_data.status == 2 else None,
+            "delivery_address": each_data.address_display
+        }
+        result.append(temp_dict)
+
+    return {
+        "data": result
+    }
+
+
+@frappe.whitelist()
+def products():
+    cookies = get_request(frappe.request)
+    fields = [
+        'idx',
+        'item_name',
+        'item_code',
+        'item_group',
+        'weightage',
+        'description'
+    ]
+
+    parts = urlparse(frappe.request.url)
+    specific = parts.path.split('/')[-1] if parts.path.split('/')[-1].find('shopee_v01.api.v1.api3.') == -1 else None
+    if specific:
+        specific = [["item_code", "=", specific]]
+
+    product_list = get_document('Item', fields=fields, cookies=cookies, filters=specific)
+    result = []
+
+    for i in product_list['data']:
+        temp_dict = {
+            "id": i['idx'],
+            "name": i['item_name'],
+            "code": i['item_code'],
+            "category_id": i['item_group'],
+            "unit_id": None,
+            "weight": i['weightage'],
+            "is_taxable": None,
+            "description": i['description']
+        }
+
+        result.append(temp_dict)
+
+    return {
+        "data": result
+    }
+
+
+@frappe.whitelist()
+def warehouse():
+    cookies = get_request(frappe.request)
+    fields = [
+        'idx',
+        'warehouse_name',
+        'name',
+        'parent',
+        'warehouse_type',
+    ]
+
+    warehouse_list = get_document('Warehouse', cookies=cookies, fields=fields)
+    result = []
+
+    for i in warehouse_list['data']:
+        warehouse_areas = get_document('Warehouse', cookies=cookies, fields=[
+            "idx",
+            "warehouse_id",
+            "name",
+            # "usage_type_id",
+            # "description",
+            "creation",
+            "owner",
+            "modified",
+            "modified_by"
+        ], filters=[["parent_warehouse", "=", i['name']]])
+
+        for j in warehouse_areas['data']:
+            j.update({
+                "usage_type_id": None,
+                "description": None
+            })
+
+        temp_dict = {
+            "id": i['idx'],
+            "name": i['warehouse_name'],
+            "code": i['name'],
+            "is_headquarter": "1" if i['parent'] is None else "0",
+            "description": None,
+            "is_store": i['warehouse_type'],
+            "default_customer_id": None,
+            "areas": warehouse_areas['data']
+        }
+
+        result.append(temp_dict)
+
+    return {
+        "data": result
+    }
+
+
+@frappe.whitelist()
+def warehouseAreas():
+    cookies = get_request(frappe.request)
+    fields = [
+        "idx",
+        "warehouse_name",
+        "name",
+        "parent",
+        "warehouse_type",
+    ]
+
+    specific = [["parent_warehouse", "!=", ""]]
+
+    parts = urlparse(frappe.request.url)
+    specific_part = parts.path.split('/')[-1] if parts.path.split('/')[-1].find(
+        'shopee_v01.api.v1.api3') == -1 else None
+    if specific_part:
+        specific += [["name", "=", specific_part]]
+
+    warehouse_areas_list = get_document('Warehouse', fields=fields, cookies=cookies, filters=specific)
+    result = []
+
+    for i in warehouse_areas_list['data']:
+        temp_dict = {
+            "id": i['idx'],
+            "warehouse_id": i['warehouse_name'],
+            "usage_type_id": None,
+            "name": i['name'],
+            "description": None,
+            "storages": None
+        }
+        result.append(temp_dict)
+
+    return {
+        "data": result
+    }
+
+
+@frappe.whitelist()
+def purchaseReceive():
+
+    '''
+    {
+    "purchase_id":"41",
+    "warehouse_id":"1",
+    "warehouse_area_id":"1",
+    "create_user_id":"2",
+    "supplier_do_number":"REFERENCE#1938764",
+    "products":
+    [
+        {
+            "purchase_product_id":"96",
+            "quantity":"100",
+            "warehouse_area_storage_id": ""
+        },
+        {
+            "purchase_product_id":"97",
+            "quantity":"250",
+            "warehouse_area_storage_id": ""
+        },
+        {
+            "purchase_product_id":"98",
+            "quantity":"2500",
+            "warehouse_area_storage_id": ""
+        },
+        {
+            "purchase_product_id":"99",
+            "quantity":"750",
+            "warehouse_area_storage_id": ""
+        }
+    ]
+}
+
+    '''
+
+    data = validate_data(frappe.request.data)
+
+
+    temp_dict = {
+        "supplier": data['supplier_do_number'],
+        "": data["purchase_id"],
+        "set_warehouse": data["warehouse_id"],
+        "": data["warehouse_area_id"],
+        "": data["create_user_id"],
+    }
+
+
+@frappe.whitelist()
+def get_label():
+    data = validate_data(frappe.request.data)
+    cookies = get_request(frappe.request)
+    fields = ['name', 'customer_name', 'company', 'address_display',
+              'company_address_display', 'total_net_weight', 'payment_terms_template',
+              'grand_total']
+    filters = [["Sales Invoice", "name", "=", data['id']]]
+    result = get_document('Sales Invoice', fields=fields, cookies=cookies, filters=filters)
+
+    filters = {
+        'parent': ['=', data['id']]
+    }
+
+    fields = ["item_name", "qty"]
+    check = query_db('Sales Invoice Item', fields=fields, filters=filters)
+    info_retrieved = result['data'][0]
+
+    pdf_binary = convert_to_pdf(
+        template=str(info_retrieved['payment_terms_template']), invoice=str(info_retrieved['name']),
+        weight=str(info_retrieved['total_net_weight']), shipping=str(info_retrieved['grand_total']),
+        to_entity=str(info_retrieved['customer_name']), from_entity=str(info_retrieved['company']),
+        address=str(info_retrieved['address_display']), address_company=str(info_retrieved['company_address_display']),
+        product_list1=check, delivery_type='Regular \nShipping', b_code=str('123456789012')
+    )
+
+    # import os
+    # with open(os.path.expanduser('/home/sid/PycharmProjects/supertal/mamba2/mamba-frappe-bench/apps/shopee_v01/shopee_v01/api/v1/test.pdf'), 'wb') as fout:
+    #      fout.write(base64.decodebytes(pdf_binary))
+
+    return {
+        "pdf_bin": pdf_binary
+    }
+
+
+@frappe.whitelist()
+def orders():
+    cookies = get_request(frappe.request)
+
+    sales_order_ids = get_document('Sales Order', cookies=cookies)
+    result = []
+
+    for i in sales_order_ids['data']:
+        each_data = frappe.get_doc(
+            'Sales Order',
+            i['name']
+        )
+
+        temp_dict = {
+            "id": each_data.idx,
+            "location_id": None,
+            "location_name": None,
+            "order_number":each_data.name,
+            "order_date":each_data.creation,
+            "original_order_id":each_data.name,
+            "type":each_data.order_type,
+            "status": each_data.delivery_status,
+            "payment_type": each_data.currency,
+            "customer_id": each_data.customer,
+            "customer_name": each_data.contact_display,
+            "total_product": each_data.total_qty,
+            "total_amount_excluding_tax": each_data.net_total,
+            "discount_amount": each_data.discount_amount,
+            "tax_amount": each_data.total_taxes_and_charges,
+            "total_amount": each_data.grand_total,
+            "products": [{
+                "id":j.idx,
+                "order_id": j.parent,
+                "product_id": j.item_code,
+                "product_name": j.item_name,
+                "product_code": j.item_code,
+                "price": j.rate,
+                "qty": j.qty,
+                "unit_id": j.item_group,
+                "discount": j.discount_amount,
+                "subtotal_amount": j.base_net_amount,
+                "product_condition_id": j.docstatus,
+                "notes": None,
+                "isActive": None
+            } for j in each_data.items],
+        }
+        result.append(temp_dict)
+
+    return {
+        "data": result
+    }
