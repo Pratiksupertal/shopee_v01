41da2ea31d708948dd37fdb0596516210dc0e858
siddhanth121 <siddhanth@supertal.io>
2021-06-25 08:19:36 +0530

    v5 label added

--- shopee_v01/api/v1/api4.py
+++ shopee_v01/api/v1/api4.py
@@ -87,6 +87,40 @@ def convert_to_pdf(template=None, invoice=None, weight=None, shipping=None, to_e
     return encoded_string
 
 
+@frappe.whitelist()
+def get_label():
+    data = validate_data(frappe.request.data)
+    fields = ['name', 'customer_name', 'company', 'address_display',
+              'company_address_display', 'total_net_weight', 'payment_terms_template',
+              'grand_total', 'owner']
+    filters = {"name": data['id']}
+    result = frappe.get_list('Sales Invoice', fields=fields, filters=filters)
+
+    filters = {
+        'parent': data['id']
+    }
+
+    fields = ["item_name", "qty"]
+    check = frappe.get_list(
+        'Sales Invoice Item',
+        fields=fields,
+        filters=filters)
+    info_retrieved = result[0]
+
+    pdf_binary = convert_to_pdf(
+        template=str(info_retrieved['payment_terms_template']), invoice=str(info_retrieved['name']),
+        weight=str(info_retrieved['total_net_weight']), shipping=str(info_retrieved['grand_total']),
+        to_entity=str(info_retrieved['customer_name']), from_entity=str(info_retrieved['company']),
+        address=str(info_retrieved['address_display']), address_company=str(info_retrieved['company_address_display']),
+        product_list1=check, delivery_type='Regular \nShipping', b_code=str('123456789012'),
+        owner=str(info_retrieved['owner'])
+    )
+
+    return {
+        "pdf_bin": str(pdf_binary)
+    }
+
+
 @frappe.whitelist(allow_guest=True)
 def login():
     data = validate_data(frappe.request.data)
