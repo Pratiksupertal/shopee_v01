021f31325d73bbbb307d8f7c8349c2e4b4516e6c
siddhanth121 <siddhanth@supertal.io>
2021-07-02 18:41:46 +0530

    traceback added

--- shopee_v01/api/v1/api4.py
+++ shopee_v01/api/v1/api4.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 import base64
 import os
 import barcode
+import traceback
 from urllib.parse import urlparse, unquote, parse_qs
 import datetime as dt
 
@@ -232,8 +233,10 @@ def products():
         query_limit = int(parse_qs(urlparse(frappe.request.url).query)['limit'][0])
         query_page = int(parse_qs(urlparse(frappe.request.url).query)['page'][0])
         each_data_list_length = len(data_list)
+        a[10]
         each_data_list = data_list[min(each_data_list_length, query_page * query_limit) : min(each_data_list_length, (query_page + 1) * query_limit)]
     except:
+        traceback.print_exc()
         each_data_list = data_list
         query_limit = len(data_list)
         query_page = 0
