eef2cfb1ad8471cc007fc869c4cc55bb6a2f9464
Pratiksupertal <pratik@supertal.io>
2021-07-15 17:29:54 +0530

    Autoname is added for Item group doctype

---
+++ shopee_v01/shopee_v01/custom_script/item_group.py
@@ -0,0 +1,17 @@
+from frappe.model.naming import parse_naming_series
+import frappe
+from frappe.model.document import Document
+
+
+def autoname(doc,method):
+    print("----- autoname is called from custom script -------")
+    if doc.is_new():
+        doc.name = make_autoname(doc.item_group_name+"-"+doc.item_group_description)
+
+def make_autoname(key="", doctype="", doc=""):
+	if key == "hash":
+		return frappe.generate_hash(doctype, 10)
+
+	parts = key.split('.')
+	n = parse_naming_series(parts, doctype, doc)
+	return n
