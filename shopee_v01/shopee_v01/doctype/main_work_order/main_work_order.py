bcef34fb56f049f0b341ffe5cd3908cb83b5682c
Pratiksupertal <pratik@supertal.io>
2021-07-16 15:45:43 +0530

    Main work order on submit purchase order is created for external production and work order is created

--- shopee_v01/shopee_v01/doctype/main_work_order/main_work_order.py
+++ shopee_v01/shopee_v01/doctype/main_work_order/main_work_order.py
@@ -10,6 +10,7 @@ from erpnext.manufacturing.doctype.bom.bom import validate_bom_no, get_bom_items
 from erpnext.stock.utils import get_bin, validate_warehouse_company, get_latest_stock_qty
 from frappe.model.naming import make_autoname
 
+
 class MainWorkOrder(Document):
 	#For main work order Art no is Doctype ART No
 	def autoname(self):
@@ -39,35 +40,66 @@ class MainWorkOrder(Document):
 
 	def before_submit(self):
 		self.submitted_by = frappe.session.user
+		for row in self.work_order_item_detail:
+			with_operation = frappe.db.get_value("BOM",row.bom,"with_operations")
+			if(self.is_external and with_operation ==0):
+				doc = frappe.new_doc("Purchase Order")
+				doc.supplier = self.supplier
+				doc.schedule_date = self.expected_finish_date
+				doc.is_subcontracted = "Yes"
+				doc.supplier_warehouse = self.supplier_warehouse
+				doc.po_type = "FACTORY"
+				item = frappe.db.get_value("BOM",row.bom,"item")
+				doc.append("items", {
+					"item_code": item ,
+					"warehouse": self.supplier_warehouse,
+					"qty": row.qty,
+					"rate": self.manufacturing_rate,
+					"schedule_date": self.expected_finish_date,
+					"bom":row.bom
+				})
+				doc.save()
+				# setting up reserve_warehouse in purchase order
+				if doc.is_subcontracted == "Yes":
+					doc.reserve_warehouse = self.supplier_warehouse
+					supp_items = doc.get("supplied_items")
+					for d in supp_items:
+						d.reserve_warehouse = self.supplier_warehouse
+				doc.save()
+				doc.submit()
 
 
 	def on_submit(self):
+		# External production will create new purchase order.
 		for row in self.work_order_item_detail:
-			doc = frappe.new_doc('Work Order')
-			# doc.production_item = row.art_no
-			doc.qty = row.qty
+			with_operation = frappe.db.get_value("BOM",row.bom,"with_operations")
+			# Internal production will create new work order.
+			if(self.is_external!=1 and with_operation ==1):
+				doc = frappe.new_doc('Work Order')
+				# doc.production_item = row.art_no
+				doc.qty = row.qty
 
-			doc.spk_date = self.spk_date
-			doc.wip_warehouse = self.wip_warehouse
-			doc.fg_warehouse = self.fg_warehouse
-			doc.scrap_warehouse = self.scrap_warehouse
-			doc.bom_no = row.bom
-			doc.company = self.company
-			bom_data = frappe.get_doc("BOM",row.bom)
-			doc.production_item = bom_data.item
-			if bom_data.with_operations:
-				for op_row in bom_data.operations:
-					print(op_row.operation)
-				sql = "select operation, description, workstation, idx,'Pending' as status,base_hour_rate as hour_rate, time_in_mins, parent as bom, batch_size from `tabBOM Operation` where parent = '{0}' order by idx".format(row.bom)
-				operations = frappe.db.sql(sql, as_dict=1)
+				doc.spk_date = self.spk_date
+				doc.wip_warehouse = self.wip_warehouse
+				doc.fg_warehouse = self.fg_warehouse
+				doc.scrap_warehouse = self.scrap_warehouse
+				doc.bom_no = row.bom
+				doc.company = self.company
+				bom_data = frappe.get_doc("BOM",row.bom)
+				doc.production_item = bom_data.item
+				if bom_data.with_operations:
+					for op_row in bom_data.operations:
+						print(op_row.operation)
+					sql = "select operation, description, workstation, idx,'Pending' as status,base_hour_rate as hour_rate, time_in_mins, parent as bom, batch_size from `tabBOM Operation` where parent = '{0}' order by idx".format(row.bom)
+					operations = frappe.db.sql(sql, as_dict=1)
 
-				doc.set('operations', operations)
-			doc.expected_delivery_date = self.expected_finish_date
-			doc.save(ignore_permissions=True)
-			frappe.db.commit()
-			doc.submit()
-		# self.save()
-		# self.docstatus=1
+					doc.set('operations', operations)
+				doc.expected_delivery_date = self.expected_finish_date
+				doc.save(ignore_permissions=True)
+				frappe.db.commit()
+				doc.submit()
+			# self.save()
+			# self.docstatus=1
 
 
 	def fetch_required_item(self,bom):
