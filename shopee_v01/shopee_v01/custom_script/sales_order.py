68a0499fb23db07cac16aec65e425a75831cd2ce
siddhanth121 <siddhanth@supertal.io>
2021-08-25 15:40:00 +0530

    added available qty in sales order from item counter - S04T08shopee_v01/fixtures/custom_field.json

--- shopee_v01/shopee_v01/custom_script/sales_order.py
+++ shopee_v01/shopee_v01/custom_script/sales_order.py
@@ -3,5 +3,7 @@ import frappe
 
 @frappe.whitelist()
 def size_filter(item_code):
-    doc = frappe.get_doc('Item', item_code)
-    return doc.invent_size_id
+    doc1 = frappe.get_doc('Item', item_code)
+    doc2 = frappe.get_doc('Item Counter')
+    doc2 = [x.available_items for x in doc2.total_item_count_in_warehouse if x.item_code == item_code]
+    return doc1.invent_size_id, doc2[0] if len(doc2) > 0 else ''
