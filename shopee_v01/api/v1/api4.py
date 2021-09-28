d4f7495c4339f31344e72c2a13393b3e6c25fc2b
siddhanth121 <siddhanth@supertal.io>
2021-09-28 07:58:51 +0530

    added sales_delivery_note api

--- shopee_v01/api/v1/api4.py
+++ shopee_v01/api/v1/api4.py
@@ -11,8 +11,9 @@ import traceback
 from urllib.parse import urlparse, unquote, parse_qs
 import datetime as dt
 from frappe.utils import today
-from erpnext.stock.doctype.pick_list.pick_list import create_stock_entry
+from erpnext.stock.doctype.pick_list.pick_list import create_stock_entry, create_delivery_note
 from erpnext.stock.doctype.material_request.material_request import create_pick_list
+from erpnext.selling.doctype.sales_order.sales_order import create_pick_list as create_pick_list_from_sales_order
 
 
 def validate_data(data):
@@ -695,4 +696,28 @@ def material_stock_entry():
     new_doc_stock_entry.save()
     new_doc_stock_entry.submit()
 
-    return format_result(result=new_doc_stock_entry.name, message='Data Created', status_code=200)
+    return format_result(result={'stock entry': new_doc_stock_entry.name}, message='Data Created', status_code=200)
+
+
+@frappe.whitelist()
+def sales_delivery_note():
+    '''
+    Create Pick List for given Sales Order
+    '''
+    data = validate_data(frappe.request.data)
+    try:
+        pick_list_sales = create_pick_list_from_sales_order(data['sales_order'])
+        pick_list_sales.save()
+        pick_list_sales.submit()
+    except:
+        return format_result(result="There was a problem creating the Pick List", message='Error', status_code=500)
+
+    '''
+    Create Delivery Note from Pick List
+    '''
+    new_delivery_note = create_delivery_note(pick_list_sales.name)
+    new_delivery_note.save()
+    new_delivery_note.submit()
+
+    return format_result(result={'delivery note': new_delivery_note.name}, message='Data Created', status_code=200)
+
