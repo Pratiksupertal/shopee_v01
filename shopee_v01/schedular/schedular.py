67652e9d66ccfb83897798730e511791bf39c9f7
Pratiksupertal <pratik@supertal.io>
2021-10-06 16:59:47 +0530

    Logger is completed for finished 901 warehouse

--- shopee_v01/schedular/schedular.py
+++ shopee_v01/schedular/schedular.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 # import hashlib
 # import requests
 from shopee_v01.shopeemarketplace_v01.utils import refresh_token
+from shopee_v01.utils import schedular_log
 
 def refresh_acess_token():
     shop_id = 220288436
@@ -57,3 +58,26 @@ def update_finished_901_item_qty_summary():
                 balance_qty, item)
             query = frappe.db.sql(sql,debug=True)
             content.append([item,balance_qty])
+
+
+def update_finished_901_item_qty_summary():
+    warehouse_list = frappe.get_doc('Finished901ItemQtySummary')
+    item_dict = {i.item_code : i.available_items for i in warehouse_list.total_item_count_in_warehouse}
+    warehouse_tuple = [i.warehouse for i in warehouse_list.child_warehouse]
+    for item in item_dict.keys():
+        balance_qty = 0
+        for i in range(len(warehouse_tuple)):
+            temp = frappe.db.sql("""select qty_after_transaction from `tabStock Ledger Entry`
+            where item_code=%s and warehouse = %s and is_cancelled='No'
+            order by posting_date desc, posting_time desc, creation desc
+            limit 1""", (item, warehouse_tuple[i]))
+            temp = int(temp[0][0]) if len(temp)>0 else 0
+            balance_qty = balance_qty + temp
+        msg_log = "Item code : {0} is checking".format(item)
+        if int(balance_qty) != int(item_dict[item]):
+            sql = "update `tabTotal Item count in Warehouse` set available_items ={0} ,modified_time = now()  where item_code = '{1}';".format(
+                balance_qty, item)
+            query = frappe.db.sql(sql,debug=True)
+            msg_log = msg_log + "\nUpdated values-> Item code : {0} ; available qty : {1}".format(item,balance_qty)
+        schedular_log(msg_log)
+    pass
