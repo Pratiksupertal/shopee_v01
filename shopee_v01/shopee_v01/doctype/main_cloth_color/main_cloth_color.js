dac938f19b6c8710efacf07fba23d16238e88af8
Pratiksupertal <pratik@supertal.io>
2021-10-13 08:46:49 +0530

    Item custumization master data

---
+++ shopee_v01/shopee_v01/doctype/main_cloth_color/main_cloth_color.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Pratik Mane and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Main Cloth Color', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ shopee_v01/shopee_v01/doctype/rise_type/rise_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Pratik Mane and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Rise Type', {
+	// refresh: function(frm) {
+
+	// }
+});
