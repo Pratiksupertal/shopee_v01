3d59dde3a3c2e54910f063e43a84f3194fab30f8
Pratiksupertal <pratik@supertal.io>
2021-10-26 16:50:04 +0530

    Barcode auto genarated with new item created

--- shopee_v01/shopee_v01/custom_script/item.py
+++ shopee_v01/shopee_v01/custom_script/item.py
@@ -2,13 +2,18 @@ import frappe
 from frappe.model.document import Document
 
 def validate(doc,method):
-    sql = "select barcode from `tabItem Barcode` order by creation desc limit 1".format(doc.item_code)
-    pre_barcode = frappe.db.sql(sql,as_dict=True)
-    barcode = int(pre_barcode[0].barcode)+1
-    if len(pre_barcode)>0:
-        doc.append("barcodes",{
-        "barcode":str(barcode)
-        })
+    if(doc.__islocal):
+        sql = "select barcode from `tabItem Barcode` order by creation desc limit 1".format(doc.item_code)
+        pre_barcode = frappe.db.sql(sql,as_dict=True)
+        barcode = int(pre_barcode[0].barcode)+1
+        if len(pre_barcode)>0:
+            doc.append("barcodes",{
+            "barcode":str(barcode)
+            })
+        else:
+            doc.append("barcodes",{
+            "barcode":"1000001"
+            })
 
 
 @frappe.whitelist()
