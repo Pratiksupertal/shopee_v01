cfb99922770bea359163fb58f1197936a8967d97
Amirul Islam <amirul@supertal.io>
2021-12-12 12:50:02 +0530

    added fields in pick list for material request api response

--- shopee_v01/api/v1/api4.py
+++ shopee_v01/api/v1/api4.py
@@ -1072,13 +1072,17 @@ def pick_list_with_mtr():
     """
     Filter by `material_request type` = [ Material Transfer | Manufacture | Material Issue ]
     """
-    material_request_list = frappe.db.get_list('Material Request',
-        filters={
-            'material_request_type': ['in', ['Material Transfer', 'Manufacture', 'Material Issue']]
-        },
-        fields=['name', 'material_request_type']
-    )
-    return material_request_list
+    material_request_list = frappe.db.sql(
+        "select name, purpose, for_qty from `tabPick List` where purpose like '%Material Transfer%' or purpose like '%Manufacture%' or purpose like '%Material Issue%';"
+        )
+    result = {}
+    for item in material_request_list:
+        pick_list_id = item[0]
+        result[pick_list_id] = {
+            "type": item[1],
+            "qty": item[2]
+        }
+    return result
     
 
 def pick_list_with_so():
