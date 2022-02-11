734a102d16e7ac98940fcc692f077fdebfc84039
Amirul Islam <amirul@supertal.io>
2022-02-11 11:16:24 +0530

    PR 220 included

--- shopee_v01/api/v1/api4/warehouse_app.py
+++ shopee_v01/api/v1/api4/warehouse_app.py
@@ -400,3 +400,40 @@ def filter_receive_at_warehouse_for_packing_area():
         return format_result(result=final_filtered_se, success=True, status_code=200, message='Data Found')
     except Exception as e:
         return format_result(result=None, success=False, status_code=400, message=str(e))
+
+
+@frappe.whitelist()
+def create_delivery_note_from_pick_list():
+    try:
+        data = validate_data(frappe.request.data)
+        pick_list_name = data.get('pick_list')
+        if not pick_list_name:
+            raise Exception('Pick List name required')
+
+        pick_list_data = frappe.db.get_value('Pick List',
+                            pick_list_name,
+                            ['name', 'customer', 'company']
+                        )
+        if not pick_list_data[0]:
+            raise Exception('Pick List name is not valid')
+
+        pick_list_items = frappe.db.get_list('Pick List Item',
+                        filters={
+                            'parent': pick_list_name,
+                            'parentfield': 'locations'
+                        },
+                        fields=['item_code', 'item_name', 'qty', 'uom', 'warehouse']
+                    )
+        print(pick_list_items)
+
+        delivery_note = frappe.new_doc('Delivery Note')
+        delivery_note.customer = pick_list_data[1]
+        delivery_note.company = pick_list_data[2]
+        delivery_note.pick_list = pick_list_name
+        for item in pick_list_items:
+            print(item)
+            delivery_note.append("items", item)
+        delivery_note.insert()        
+        return format_result(result=delivery_note, success=True, message='Delivery Note successfully created', status_code=200)
+    except Exception as e:
+        return format_result(success=False, status_code=400, message=str(e), exception=str(e))
