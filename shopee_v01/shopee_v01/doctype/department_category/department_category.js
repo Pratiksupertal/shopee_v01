d7ed884196a9a7ed5715c5479994e3dee0141bfb
rajib <rajib@supertal.io>
2022-02-14 11:24:20 +0530

    Server Error: City,Department_category,region,and store has been added

---
+++ shopee_v01/shopee_v01/doctype/department_category/department_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Pratik Mane and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Department Category', {
+	// refresh: function(frm) {
+
+	// }
+});
