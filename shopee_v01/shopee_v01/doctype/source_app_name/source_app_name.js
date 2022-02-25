ca3b73316819bf2f2ccd6acf83c7f41a9876537e
Amirul Islam <amirul@supertal.io>
2022-02-25 12:18:57 +0530

    added new doctype source app name

---
+++ shopee_v01/shopee_v01/doctype/source_app_name/source_app_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Pratik Mane and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Source App Name', {
+	// refresh: function(frm) {
+
+	// }
+});
