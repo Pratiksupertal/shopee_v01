cff1b49a61de3ffbbdf036216b4dc85bdf2fd92c
Widjo2021 <widjojono2010@gmail.com>
2022-03-31 02:25:34 +0700

    Modify sales order doctype for hidden field 31-03-2022

--- shopee_v01/shopee_v01/custom_script/sales_order.js
+++ shopee_v01/shopee_v01/custom_script/sales_order.js
@@ -17,35 +17,26 @@ frappe.ui.form.on('Sales Order Item', {
 });
 
 frappe.ui.form.on("Sales Order",{ before_load:function(frm) {
-  var df=frappe.meta.get_docfield("Sales Order Item", "rate",frm.doc.name);
-  var df2=frappe.meta.get_docfield("Sales Order Item", "amount",frm.doc.name);
-  var df3=frappe.meta.get_docfield("Sales Order Item", "basic_rate",frm.doc.name);
-  var df4=frappe.meta.get_docfield("Payment Schedule", "payment_amount",frm.doc.name);
-  var df5=frappe.meta.get_docfield("Sales Taxes and Charges", "tax_amount",frm.doc.name);
-  var df6=frappe.meta.get_docfield("Sales Taxes and Charges", "total",frm.doc.name);
-  var df7=frappe.meta.get_docfield("Sales Team", "allocated_amount",frm.doc.name);
-  var df8=frappe.meta.get_docfield("Sales Order Item", "price_list_rate",frm.doc.name);
-  var df9=frappe.meta.get_docfield("Sales Order Item", "discount_and_margin",frm.doc.name);
-  var df10=frappe.meta.get_docfield("Sales Order Item", "rate_with_margin",frm.doc.name);
-  var df11=frappe.meta.get_docfield("Sales Order Item", "discount_amount",frm.doc.name);
-  var df12=frappe.meta.get_docfield("Sales Order Item", "net_rate",frm.doc.name);
-  var df13=frappe.meta.get_docfield("Sales Order Item", "net_amount",frm.doc.name);
-  var df14=frappe.meta.get_docfield("Sales Order Item", "valuation_rate",frm.doc.name);
-  var df15=frappe.meta.get_docfield("Sales Order Item", "billed_amt",frm.doc.name);
-  var df16=frappe.meta.get_docfield("Sales Order Item", "gross_profit",frm.doc.name);
-  var df17=frappe.meta.get_docfield("Sales Order Item", "blanket_order_rate",frm.doc.name);
+  var df=frappe.meta.get_docfield("Sales Order Item", "rate",cur_frm.doc.name);
+  var df2=frappe.meta.get_docfield("Sales Order Item", "amount",cur_frm.doc.name);
+  var df3=frappe.meta.get_docfield("Sales Order Item", "basic_rate",cur_frm.doc.name);
+  var df4=frappe.meta.get_docfield("Payment Schedule", "payment_amount",cur_frm.doc.name);
+  var df5=frappe.meta.get_docfield("Sales Taxes and Charges", "tax_amount",cur_frm.doc.name);
+  var df6=frappe.meta.get_docfield("Sales Taxes and Charges", "total",cur_frm.doc.name);
+  var df7=frappe.meta.get_docfield("Sales Team", "allocated_amount",cur_frm.doc.name);
+  var df8=frappe.meta.get_docfield("Sales Order Item", "price_list_rate",cur_frm.doc.name);
+  var df9=frappe.meta.get_docfield("Sales Order Item", "discount_and_margin",cur_frm.doc.name);
+  var df10=frappe.meta.get_docfield("Sales Order Item", "rate_with_margin",cur_frm.doc.name);
+  var df11=frappe.meta.get_docfield("Sales Order Item", "discount_amount",cur_frm.doc.name);
+  var df12=frappe.meta.get_docfield("Sales Order Item", "net_rate",cur_frm.doc.name);
+  var df13=frappe.meta.get_docfield("Sales Order Item", "net_amount",cur_frm.doc.name);
+  var df14=frappe.meta.get_docfield("Sales Order Item", "valuation_rate",cur_frm.doc.name);
+  var df15=frappe.meta.get_docfield("Sales Order Item", "billed_amt",cur_frm.doc.name);
+  var df16=frappe.meta.get_docfield("Sales Order Item", "gross_profit",cur_frm.doc.name);
+  var df17=frappe.meta.get_docfield("Sales Order Item", "blanket_order_rate",cur_frm.doc.name);
   if (frappe.user_roles.includes("Accounts Manager") || frappe.user_roles.includes("Accounting Supervisor") ||
        frappe.user_roles.includes("CEO")) {
-     frm.set_df_property("total", "hidden", 0);
-     frm.set_df_property("base_net_total", "hidden", 0);
-     frm.set_df_property("net_total", "hidden", 0);
-         frm.set_df_property("total_taxes_and_charges", "hidden", 0);
-         frm.set_df_property("discount_amount", "hidden", 0);
-         frm.set_df_property("grand_total", "hidden", 0);
-         frm.set_df_property("rounding_adjustment", "hidden", 0);
-         frm.set_df_property("rounded_total", "hidden", 0);
-         frm.set_df_property("in_words", "hidden", 0);
-         frm.set_df_property("advance_paid", "hidden", 0);
+
      df.hidden=0;
      df2.hidden=0;
      df3.hidden=0;
@@ -63,16 +54,17 @@ frappe.ui.form.on("Sales Order",{ before_load:function(frm) {
      df15.hidden=0;
      df16.hidden=0;
      df17.hidden=0;
-     frm.set_df_property("total", "hidden", 0);
-     frm.set_df_property("base_net_total", "hidden", 0);
-     frm.set_df_property("net_total", "hidden", 0);
-     frm.set_df_property("total_taxes_and_charges", "hidden", 0);
-     frm.set_df_property("discount_amount", "hidden", 0);
-     frm.set_df_property("grand_total", "hidden", 0);
-     frm.set_df_property("rounding_adjustment", "hidden", 0);
-     frm.set_df_property("rounded_total", "hidden", 0);
-     frm.set_df_property("in_words", "hidden", 0);
-     frm.set_df_property("advance_paid", "hidden", 0);
+     cur_frm.set_df_property("total", "hidden", 0);
+     cur_frm.set_df_property("net_total", "hidden", 0);
+     cur_frm.set_df_property("total_taxes_and_charges", "hidden", 0);
+     cur_frm.set_df_property("other_charges_calculation", "hidden", 0);
+     cur_frm.set_df_property("discount_amount", "hidden", 0);
+     cur_frm.set_df_property("grand_total", "hidden", 0);
+     cur_frm.set_df_property("rounding_adjustment", "hidden", 0);
+     cur_frm.set_df_property("rounded_total", "hidden", 0);
+     cur_frm.set_df_property("in_words", "hidden", 0);
+     cur_frm.set_df_property("advance_paid", "hidden", 0);
+     cur_frm.set_df_property("total_commision", "hidden", 0);
 
   }
   else {
@@ -93,16 +85,17 @@ frappe.ui.form.on("Sales Order",{ before_load:function(frm) {
      df15.hidden=1;
      df16.hidden=1;
      df17.hidden=1;
-     frm.set_df_property("total", "hidden", 1);
-     frm.set_df_property("base_net_total", "hidden", 1);
-     frm.set_df_property("net_total", "hidden", 1);
-     frm.set_df_property("total_taxes_and_charges", "hidden", 1);
-     frm.set_df_property("discount_amount", "hidden", 1);
-     frm.set_df_property("grand_total", "hidden", 1);
-     frm.set_df_property("rounding_adjustment", "hidden",1);
-     frm.set_df_property("rounded_total", "hidden", 1);
-     frm.set_df_property("in_words", "hidden", 1);
-     frm.set_df_property("advance_paid", "hidden", 1);
+     cur_frm.set_df_property("total", "hidden", 1);
+     cur_frm.set_df_property("net_total", "hidden", 1);
+     cur_frm.set_df_property("total_taxes_and_charges", "hidden", 1);
+     cur_frm.set_df_property("other_charges_calculation", "hidden", 1);
+     cur_frm.set_df_property("discount_amount", "hidden", 1);
+     cur_frm.set_df_property("grand_total", "hidden", 1);
+     cur_frm.set_df_property("rounding_adjustment", "hidden",1);
+     cur_frm.set_df_property("rounded_total", "hidden", 1);
+     cur_frm.set_df_property("in_words", "hidden", 1);
+     cur_frm.set_df_property("advance_paid", "hidden", 1);
+     cur_frm.set_df_property("total_commision", "hidden", 1);
 
   }
   frm.refresh_fields();
