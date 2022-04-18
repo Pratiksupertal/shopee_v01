779aa5a7237820525b3f21d7c8118b1b6517e325
Amirul Islam <amirul@supertal.io>
2022-04-18 16:58:35 +0530

    experiment execution  time ->  with only child and parent-child warehouse

--- shopee_v01/api/v1/api4/warehouse.py
+++ shopee_v01/api/v1/api4/warehouse.py
@@ -6,6 +6,12 @@ from shopee_v01.api.v1.helpers import get_last_parameter
 
 @frappe.whitelist()
 def warehouses():
+    """
+    Using in Mobile App (flow: login -> Receiving -> PO -> Supplier DO -> Target warehouse)
+    There will show all the warehouse mapped with logged in user (Warehouse and User Mapping)
+
+    TODO: Optimization needed for this API
+    """
     fields = [
         'idx',
         'warehouse_name',
@@ -37,6 +43,8 @@ def warehouses():
             "modified_by"
         ], filters={'parent_warehouse': i['name']})
 
+        is_user_parent_warehouse = True if i['name'] in user_warehouses else False
+
         temp_dict = {
             "id": str(i['idx']),
             "name": i['warehouse_name'],
@@ -52,13 +60,39 @@ def warehouses():
                 'update_user_id': j['modified_by'],
                 'usage_type_id': None,
                 'description': None
-            } for j in warehouse_areas if j['name'] in user_warehouses]
+            } for j in warehouse_areas if is_user_parent_warehouse or j['name'] in user_warehouses]
         }
 
         if len(temp_dict.get('areas', [])):
             result.append(temp_dict)
 
     return format_result(result=result, status_code=200, message='Data Found')
+
+
+@frappe.whitelist()
+def warehouses_():
+    """
+    Using in Mobile App (flow: login -> Receiving -> PO -> Supplier DO -> Target warehouse)
+    There will show all the warehouse mapped with logged in user (Warehouse and User Mapping)
+
+    TODO: Optimization needed for this API
+    """
+    
+    user = frappe.session.user
+    user_warehouses = frappe.db.sql(
+        "SELECT warehouse_id FROM `tabUser Warehouse Mapping` where user_id='{}'"
+        .format(user))
+    user_warehouses = [warehouse[0] for warehouse in user_warehouses]
+    print(user_warehouses)
+    
+    fields = [
+        'name',
+        'parent'
+    ]
+    
+    warehouse_list = frappe.db.get_list('Warehouse', fields=fields, filters={'name': ['in', user_warehouses]})
+
+    return format_result(result=warehouse_list, status_code=200, message='Data Found')
 
 
 @frappe.whitelist()
