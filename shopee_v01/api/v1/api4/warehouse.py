c0d46647d3181454e60d040fb0bb65151c4a2fa0
Amirul Islam <amirul@supertal.io>
2022-04-19 14:40:05 +0530

    ISS-258 || Refactoring and Redesigning the warehouse list api || Added User Warehouse Mapping

--- shopee_v01/api/v1/api4/warehouse.py
+++ shopee_v01/api/v1/api4/warehouse.py
@@ -2,6 +2,7 @@ import frappe
 
 from shopee_v01.api.v1.helpers import format_result
 from shopee_v01.api.v1.helpers import get_last_parameter
+from shopee_v01.api.v1.helpers import get_user_mapped_warehouses
 
 
 @frappe.whitelist()
@@ -9,90 +10,29 @@ def warehouses():
     """
     Using in Mobile App (flow: login -> Receiving -> PO -> Supplier DO -> Target warehouse)
     There will show all the warehouse mapped with logged in user (Warehouse and User Mapping)
-
-    TODO: Optimization needed for this API
     """
-    fields = [
-        'idx',
-        'warehouse_name',
-        'name',
-        'parent',
-        'warehouse_type',
-    ]
-
-    warehouse_list = frappe.get_list('Warehouse', fields=fields)
-    result = []
-
-    user = frappe.session.user
-    user_warehouses = frappe.db.sql(
-        "SELECT warehouse_id FROM `tabUser Warehouse Mapping` where user_id='{}'"
-        .format(user))
-    user_warehouses = [warehouse[0] for warehouse in user_warehouses]
-    print(user_warehouses)
-
-    for i in warehouse_list:
-        warehouse_areas = frappe.get_list('Warehouse', fields=[
+    user_warehouses = get_user_mapped_warehouses()
+    warehouse_areas = frappe.get_list(
+        'Warehouse',
+        fields=[
             "idx",
-            "warehouse_id",
             "name",
-            # "usage_type_id",
-            # "description",
+            "warehouse_id",
             "creation",
             "owner",
             "modified",
-            "modified_by"
-        ], filters={'parent_warehouse': i['name']})
-
-        is_user_parent_warehouse = True if i['name'] in user_warehouses else False
-
-        temp_dict = {
-            "id": str(i['idx']),
-            "name": i['warehouse_name'],
-            "code": i['name'],
-            "description": None,
-            "areas": [{
-                'id': j['idx'],
-                'warehouse_id': j['warehouse_id'],
-                'name': j['name'],
-                'create_time': j['creation'],
-                'update_time': j['modified'],
-                'create_user_id': j['owner'],
-                'update_user_id': j['modified_by'],
-                'usage_type_id': None,
-                'description': None
-            } for j in warehouse_areas if is_user_parent_warehouse or j['name'] in user_warehouses]
+            "modified_by",
+            "parent"
+        ],
+        filters={
+            "name": ["in", user_warehouses]
         }
-
-        if len(temp_dict.get('areas', [])):
-            result.append(temp_dict)
-
-    return format_result(result=result, status_code=200, message='Data Found')
-
-
-@frappe.whitelist()
-def warehouses_():
-    """
-    Using in Mobile App (flow: login -> Receiving -> PO -> Supplier DO -> Target warehouse)
-    There will show all the warehouse mapped with logged in user (Warehouse and User Mapping)
-
-    TODO: Optimization needed for this API
-    """
-    
-    user = frappe.session.user
-    user_warehouses = frappe.db.sql(
-        "SELECT warehouse_id FROM `tabUser Warehouse Mapping` where user_id='{}'"
-        .format(user))
-    user_warehouses = [warehouse[0] for warehouse in user_warehouses]
-    print(user_warehouses)
-    
-    fields = [
-        'name',
-        'parent'
-    ]
-    
-    warehouse_list = frappe.db.get_list('Warehouse', fields=fields, filters={'name': ['in', user_warehouses]})
-
-    return format_result(result=warehouse_list, status_code=200, message='Data Found')
+    )
+    return format_result(
+        result=warehouse_areas,
+        status_code=200,
+        message='Data Found'
+    )
 
 
 @frappe.whitelist()
@@ -125,4 +65,8 @@ def warehouseAreas():
         }
         result.append(temp_dict)
 
-    return format_result(result=result, status_code=200, message='Data Found')
+    return format_result(
+        result=result,
+        status_code=200,
+        message='Data Found'
+    )
