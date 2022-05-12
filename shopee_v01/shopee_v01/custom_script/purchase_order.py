627d2c1e7681ccf7697cc1b7e73cc507a51ab21b
Kunal <kunalk@supertal.io>
2022-05-12 19:58:00 +0530

    Same code with conflicts fixed for ISS-271

--- shopee_v01/shopee_v01/custom_script/purchase_order.py
+++ shopee_v01/shopee_v01/custom_script/purchase_order.py
@@ -1,8 +1,10 @@
 from frappe.model.naming import make_autoname
-import frappe
+import frappe, json
 from frappe.model.document import Document
 from frappe.utils import cstr, flt, getdate, new_line_sep, nowdate, add_days
 from frappe.model.mapper import get_mapped_doc
+from frappe.desk.notifications import get_notification_config
+from frappe import _
 
 
 def autoname(doc,method):
@@ -69,3 +71,47 @@ def make_stock_entry_material_request(source_name, target_doc=None):
 
     return doclist
 
+
+@frappe.whitelist()
+def create_pick_list(source_name, input_qty=0, total_qty=0):
+    try:
+        response = ""
+        input_qty = int(input_qty)
+        total_qty = int(total_qty)
+        all_items = []
+        purchase_order_doc = frappe.get_doc('Purchase Order', source_name)
+        rows = purchase_order_doc.items
+        for row in rows:
+            all_items.append(row.item_name)
+        max_qty = total_qty/len(all_items)
+        if input_qty > max_qty:
+            message = f"Pick List not created for purchase Order input quantity entered is greater than max quantity"
+            return frappe.msgprint(message)
+        picklist = generate_new_pick_list(all_items, purchase_order_doc, max_qty, input_qty)
+        if picklist:
+            response += f"<br>Pick List <strong><a href='{picklist.get_url()}'>{picklist.get('name')}</a></strong> is created for Purchase Order"
+        if not input_qty:
+            response += "Enter some integer input greater than 0 and less than or equal to Max Quantity."
+        return frappe.msgprint(response)
+    except Exception as e:
+        purchase_order_doc = frappe.get_doc('Purchase Order', source_name)
+        message = f"Pick List not created for Purchase Order - <strong>{purchase_order_doc.name}</strong>. Reason - {str(e)}"
+        return frappe.msgprint(message)
+
+
+def generate_new_pick_list(item_list, purchase_order_doc, max_qty, input_qty):
+    if not item_list or not input_qty: return
+    pick_list = frappe.new_doc('Pick List')
+    pick_list.company = purchase_order_doc.company
+    pick_list.purpose = "Material Transfer for Manufacture"
+    pick_list.purchase_order = purchase_order_doc.name
+    pick_list.for_qty = input_qty
+    for item in item_list:
+        row = pick_list.append('locations', {})
+        row.item_code = item
+        row.warehouse = "Stores - ISS"
+        row.qty = input_qty
+        row.stock_qty = input_qty
+        row.picked_qty = input_qty
+    pick_list.save()
+    return pick_list
