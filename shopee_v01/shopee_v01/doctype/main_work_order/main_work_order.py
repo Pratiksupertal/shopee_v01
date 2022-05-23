68250e4c03f201ae0f80bef9e0f34a0f8fd58d8f
Pratiksupertal <pratik@supertal.io>
2022-05-23 12:08:22 +0530

    Reverting FOB task for now, will again add to dev server once staging and production server is updated

--- shopee_v01/shopee_v01/doctype/main_work_order/main_work_order.py
+++ shopee_v01/shopee_v01/doctype/main_work_order/main_work_order.py
@@ -91,25 +91,25 @@ class MainWorkOrder(Document):
 					supp_items = doc.get("supplied_items")
 					supplied_qty = {}
 					doc.total_supplied_amount = 0
-					for i,val in enumerate(self.required_item):
-						supplied_qty[val.item_code]= val.supplied_qty
+					# for i,val in enumerate(self.required_item):
+					# 	supplied_qty[val.item_code]= val.supplied_qty
 					for d in supp_items:
-						d.supplied_qty = supplied_qty[d.rm_item_code]
+						# d.supplied_qty = supplied_qty[d.rm_item_code]
 						d.reserve_warehouse = self.source_warehouse
-						d.supplied_qty_amount = d.supplied_qty * d.rate
-						d.rate = 5000
-						doc.total_supplied_amount = doc.total_supplied_amount + d.supplied_qty_amount
-					doc.total_supplied_amount_with_tax = doc.total_supplied_amount + (doc.total_supplied_amount * tax_template.taxes[0].rate)
-					
+						# d.supplied_qty_amount = d.supplied_qty * d.rate
+						# d.rate = 5000
+					# 	doc.total_supplied_amount = doc.total_supplied_amount + d.supplied_qty_amount
+					# doc.total_supplied_amount_with_tax = doc.total_supplied_amount + (doc.total_supplied_amount * tax_template.taxes[0].rate)
+
 				doc.save()
 				doc.submit()
-				doc1 = frappe.get_doc("Purchase Order", doc.name)
-				doc1.title     # I not sure what to type here
-				doc1.grand_total = doc.grand_total - doc.total_supplied_amount_with_tax
-				doc1.rounded_total = doc1.grand_total
-				from frappe.utils import money_in_words
-				doc1.in_words = money_in_words(doc1.grand_total, "IDR")
-				doc1.save()
+				# doc1 = frappe.get_doc("Purchase Order", doc.name)
+				# doc1.title     # I not sure what to type here
+				# doc1.grand_total = doc.grand_total - doc.total_supplied_amount_with_tax
+				# doc1.rounded_total = doc1.grand_total
+				# from frappe.utils import money_in_words
+				# doc1.in_words = money_in_words(doc1.grand_total, "IDR")
+				# doc1.save()
 
 	def on_submit(self):
 		# External production will create new purchase order.
