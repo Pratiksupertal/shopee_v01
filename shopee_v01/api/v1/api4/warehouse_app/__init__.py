8221e7c67d0a347b77878f40dd5c9ece31a9e267
Kunal <kunalk@supertal.io>
2022-07-13 19:43:28 +0530

    Creating filter for Pick List using Material Request.

--- shopee_v01/api/v1/api4/warehouse_app/__init__.py
+++ shopee_v01/api/v1/api4/warehouse_app/__init__.py
@@ -7,6 +7,7 @@
 # pylint: skip-file
 
 from shopee_v01.api.v1.api4.warehouse_app.pick_list import filter_picklist
+from shopee_v01.api.v1.api4.warehouse_app.pick_list import filter_picklist_from_material_request
 from shopee_v01.api.v1.api4.warehouse_app.pick_list import picklist_details_for_warehouse_app
 from shopee_v01.api.v1.api4.warehouse_app.pick_list import assign_picker
 from shopee_v01.api.v1.api4.warehouse_app.pick_list import save_picklist_and_create_stockentry
