faef14ca5542cafabcaae7b613e2a5d0071c8e58
Widjo2021 <widjojono2010@gmail.com>
2022-07-22 18:35:57 +0700

    ISS-412B : Resolving Conflict

---
+++ shopee_v01/shopee_v01/custom_script/delivery_note_list.js
@@ -0,0 +1,14 @@
+frappe.listview_settings['Delivery Note'] = {
+  add_fields: ["sales_order","customer", "customer_name", "base_grand_total", "per_installed", "per_billed",
+   "transporter_name", "grand_total", "is_return", "status", "currency"],
+  onload(listview) {
+      console.log("Testing");
+      frappe.call({
+				method: "shopee_v01.shopee_v01.custom_script.delivery_note_list.update_table_delivery_note",
+				args: {
+					name: ""
+				}
+			});
+  },
+
+}
