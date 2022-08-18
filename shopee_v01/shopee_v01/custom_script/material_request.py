39d830fc5fa5adac83174126c48c4ab6b1ea0024
Widjo2021 <widjojono2010@gmail.com>
2022-08-18 17:43:49 +0700

    ISS-456 : Material Request, Actual Available quantity

--- shopee_v01/shopee_v01/custom_script/material_request.py
+++ shopee_v01/shopee_v01/custom_script/material_request.py
@@ -13,7 +13,7 @@ def size_filter(item_code,warehouse,qty):
     price_list = frappe.get_doc('Item Price',{"selling":1,"item_code":item_code})
     doc2 = frappe.get_doc('Finished901ItemQtySummary')
     doc2 = [x.available_items for x in doc2.total_item_count_in_warehouse if x.item_code == item_code]
-    reserved_qty2 = get_reserved_qty2(item_code,warehouse)+int(qty)
+    reserved_qty2 = get_reserved_qty2(item_code,warehouse)+int(qty)+get_reserved_qty4(item_code)
     return doc1.invent_size_id,price_list.price_list_rate, doc2[0] if len(doc2) > 0 else '', reserved_qty2
 
 @frappe.whitelist()
@@ -22,7 +22,7 @@ def actual_available_qty_schedule_date(item_code,warehouse,schedule_date,actual_
     reserved_qty2 = get_reserved_qty2(item_code,warehouse)+int(qty)
     doc1 = frappe.get_doc('Item', item_code)
     if (get_reserved_qty3(item_code,warehouse,schedule_date,actual_available_qty,qty) > 0):
-        reserved_qty3 = reserved_qty3 + int(actual_available_qty) - int(qty)
+        reserved_qty3 = reserved_qty3 + int(actual_available_qty) - int(qty) + get_reserved_qty4(item_code)
 
     else:
         doc2 = frappe.get_doc('Finished901ItemQtySummary')
@@ -61,12 +61,21 @@ def get_reserved_qty2(item_code, warehouse):
     reserved_qty = frappe.db.sql("""
 		select
 			sum(reserved_qty) as reserved_qty
-		from `tabBin` where item_code = %s and warehouse = %s""", (item_code, warehouse))
+		from `tabBin` where item_code = %s and SUBSTRING(warehouse,1,3) =%s""", (item_code, '901'))
     return flt(reserved_qty[0][0]) if reserved_qty else 0
 
 def get_reserved_qty3(item_code, warehouse, schedule_date, actual_available_qty,qty):
     """warehouse is hard coded as per Mr. Albert's instructions"""
     sql = "select actual_available_qty from `tabMaterial Request Item` where item_code = '{0}' and warehouse = '{1}' and schedule_date = '{2}' and actual_available_qty = {3}".format(item_code,warehouse,schedule_date,actual_available_qty)
+    reserved_qty = frappe.db.sql(sql)
+    FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+    logging.basicConfig(format=FORMAT)
+    logging.warning('Checking for Query %s',sql)
+    return flt(reserved_qty[0][0]) if reserved_qty else 0
+
+def get_reserved_qty4(item_code):
+    """warehouse is hard coded as per Mr. Albert's instructions"""
+    sql = "select b.reserved_qty FROM `tabItem` a LEFT JOIN `tabBin` b ON a.item_code = b.item_code LEFT JOIN `tabSales Order Item` c ON a.item_code = c.item_code LEFT JOIN `tabItem Price` d ON a.item_code = d.item_code where a.item_code = '{0}'".format(item_code)
     reserved_qty = frappe.db.sql(sql)
     FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
     logging.basicConfig(format=FORMAT)
