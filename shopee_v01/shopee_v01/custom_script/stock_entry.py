88dbe32ac4dbd90f126d9a6b037ed9cd6f6f2042
Amirul Islam <amirul@supertal.io>
2022-08-24 16:33:57 +0530

    Halosis Stock Update | get auth token function send to new auth.py file in helpers folder

--- shopee_v01/shopee_v01/custom_script/stock_entry.py
+++ shopee_v01/shopee_v01/custom_script/stock_entry.py
@@ -1,4 +1,10 @@
-import frappe, json
+import frappe
+import requests
+import json
+
+from shopee_v01.helpers.auth import get_auth_token
+
+
 group_warehouse, node_warehouse = [], []
 
 def update_finished901itemsummary(doc,action):
@@ -105,25 +111,6 @@ def get_qty(type, cal_type, item):
 
 
 def update_stock_to_halosis(doc):
-    def get_auth_token(config):
-        try:
-            url = config.base_url + 'auth'
-            data = {
-                "username": config.username,
-                "password": config.get_password('password')
-            }
-            auth_res = requests.post(url.replace("'", '"'), data=data)
-            auth_res_json = json.loads(auth_res.text)
-            auth_token = "Bearer " + auth_res_json["data"]["token"]
-            print(auth_res_json)
-            return auth_token
-        except Exception:
-            raise
-            frappe.log_error(title="Update stock API Login part", message=frappe.get_traceback())
-            frappe.msgprint(f'Problem in halosis update. {frappe.get_traceback()}')
-
-    import requests
-    # Comparing the parent warehouse
     request = []
     config = frappe.get_single("Online Warehouse Configuration")
 
