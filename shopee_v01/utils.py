8419da75c554a6f8f0fca127992641cfd9c66505
abc <rakesh@supertal.io>
2022-09-16 13:19:07 +0530

    comment to identify the condition

--- shopee_v01/utils.py
+++ shopee_v01/utils.py
@@ -18,6 +18,7 @@ def schedular_log(msg):
 
 def get_permission_query_conditions_for_item(user):
 	roles = frappe.get_roles(user)
+	# IF Roles is ISS Follow Up Supervisor or ISS Follow Up Admin then allow to view only the "parent_item_group 008 - Finish Good"
 	if (user != "Administrator") and ("ISS Follow Up Supervisor" in roles) or("ISS Follow Up Admin" in roles):
 		item_groups = frappe.get_list("Item Group", filters={ "parent_item_group": "008 - Finish Good" },
 		fields=["name"],
@@ -25,6 +26,8 @@ def get_permission_query_conditions_for_item(user):
 		final_item_group = [ '%s'%item_group.get("name") for item_group in item_groups ]
 		final_item_group = tuple(final_item_group)
 		return """(`tabItem`.item_group  in {0})""".format(final_item_group)
+
+	# IF Roles is ISS Purchasing Admin or ISS Purchasing Manager then allow to view only the "parent_item_group 001 - Raw Material,002 - Accessories"
 	elif (user != "Administrator") and ("ISS Purchasing Admin" in roles) or ("ISS Purchasing Manager" in roles):
 		item_groups = frappe.get_list("Item Group", filters={ "parent_item_group":('in', ("001 - Raw Material","002 - Accessories"))},
 		fields=["name"],
@@ -32,6 +35,8 @@ def get_permission_query_conditions_for_item(user):
 		final_item_group = [ '%s'%item_group.get("name") for item_group in item_groups ]
 		final_item_group = tuple(final_item_group)
 		return """(`tabItem`.item_group  in {0})""".format(final_item_group)
+
+	# IF Roles is ISS CEO or ISS Accounting Supervisor then allow to view only the "parent_item_group 001 - Raw Material,002 - Accessories, 008 - Finish Good"
 	elif (user != "Administrator") and ("ISS CEO" in roles) or ("ISS Accounting Supervisor" in roles):
 		item_groups = frappe.get_list("Item Group", filters={ "parent_item_group":('in', ("001 - Raw Material","002 - Accessories", "008 - Finish Good"))},
 		fields=["name"],
