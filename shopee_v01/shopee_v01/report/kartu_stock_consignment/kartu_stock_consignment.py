2247be846bee39dc8a124b9677e461f9e5929532
Widjo2021 <widjojono2010@gmail.com>
2022-11-17 04:59:57 +0700

    ISS-KartuStock-AddingActualQty-Consignment

--- shopee_v01/shopee_v01/report/kartu_stock_consignment/kartu_stock_consignment.py
+++ shopee_v01/shopee_v01/report/kartu_stock_consignment/kartu_stock_consignment.py
@@ -159,7 +159,7 @@ def get_entries(filters):
 
 
     entries = frappe.db.sql("""
-        select b.item_code, b.item_name,b.size_group,b.item_group,b.warehouse,concat(b.item_name,b.warehouse) as compare_name,iav.abbr as attribute_value,convert(b.stock_akhir,int) stock_akhir,ad.state,ad.city from (select a.item_name,
+        select k.item_code, k.item_name,k.size_group,k.item_group,k.warehouse,k.compare_name,k.attribute_value,k.stock_akhir,k.state,k.city from (select b.item_code, b.item_name,b.size_group,b.item_group,b.warehouse,concat(b.item_name,b.warehouse) as compare_name,iav.abbr as attribute_value,convert(b.stock_akhir,int) stock_akhir,ad.state,ad.city from (select a.item_name,
         a.item_code,a.size_group,a.item_group,a.division_group,a.retail_group,a.price_list,
         sum(a.actual_qty)+sum(a.planned_qty)-sum(a.reserved_qty) as stock_akhir,replace(a.warehouse,' - ISS','') as warehouse
         from ( SELECT a.item_name ,a.item_code , a.size_group, a.item_group , a.division_group ,a.retail_group ,a.brand ,format(max(d.price_list_rate),0) as price_list,
@@ -167,13 +167,13 @@ def get_entries(filters):
         b.warehouse FROM `tabItem` a LEFT JOIN `tabBin` b ON a.item_code = b.item_code LEFT JOIN `tabSales Order Item` c
         ON a.item_code = c.item_code LEFT JOIN `tabItem Price` d ON a.item_code = d.item_code where
         a.item_group in ('J2B','C2B','JC2C','F2B','F2C','JC2B','F2A','U2B','C2C','J2C','J2A',
-        'C2A','U2A','L2C','L2B','L2A','JC2A','Y2A','GIFT','2B','2A') and (a.item_code like '%.C%' or a.item_code like '.%C%' or a.item_code like '%.A%' or a.item_code like '.%A%') {0}
+        'C2A','U2A','L2C','L2B','L2A','JC2A','Y2A','GIFT','2B','2A') {0}
         group by a.item_code, b.warehouse) a where a.reserved_qty > 0 or a.actual_qty > 0 or a.projected_qty > 0
         group by a.item_name,a.item_code,a.item_group,a.division_group,a.retail_group,a.price_list,a.warehouse
         order by a.item_name asc) b left join `tabAddress` ad on b.warehouse = substring(ad.name,1,length(b.warehouse))
         inner join `tabItem Variant Attribute` iva on b.item_code = iva.parent
         inner join `tabItem Attribute Value` iav on iva.attribute_value = iav.attribute_value
-        where iva.attribute = 'Size' {1} order by b.item_name,b.warehouse,b.size_group,iav.abbr""".format(conditions2,conditions), as_dict=1)
+        where iva.attribute = 'Size' {1} order by b.item_name,b.warehouse,b.size_group,iav.abbr) k where k.item_code REGEXP '.C' or k.item_code REGEXP '.A'""".format(conditions2,conditions), as_dict=1)
 
     return entries
 
